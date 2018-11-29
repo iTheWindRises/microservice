d665dd5356dbde67bfe50204101020d31204a195
iTheWindRises <425198161@qq.com>
2018-11-29 19:24:21 +0800

    sdg

---
+++ course-duboo-service/src/main/java/com/zwj/course/Test.java
@@ -0,0 +1,14 @@
+package com.zwj.course;
+
+/**
+ * @program micro-service
+ * @description:
+ * @author: wj
+ * @create: 2018-11-29  19:13
+ */
+public class Test {
+
+    private String hhh(Integer sum) {
+        return sum.toString();
+    }
+}
